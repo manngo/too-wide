053cd0810d69ed3c0e7212c1bdc415a6dd410337
michaelmutert <michael.mutert@rwth-aachen.de>
2020-06-01 10:30:21 +0200

    Reformat files and fix context menu not appearing

--- fix-width.js
+++ fix-width.js
@@ -1,25 +1,28 @@
 //	Toggle Width
-	function toggleWidth(request, sender, sendResponse) {
-	//	var style=document.body.style;
-		if('oldCSS' in document.body.style) {	//	turn off
-			document.body.style.cssText=document.body.style.oldCSS;
-			delete document.body.style.oldCSS;
-		}
-		else {									//	turn on
-			document.body.style.oldCSS=document.body.style.cssText;
-			var getting = browser.storage.local.get('maxWidth');
-			getting.then(onGot, onError);
-		}
-		browser.runtime.onMessage.removeListener(toggleWidth);
+function toggleWidth(request, sender, sendResponse) {
+//	var style=document.body.style;
+	
+	if ('oldCSS' in document.body.style) {
+		//	turn off
+		document.body.style.cssText = document.body.style.oldCSS;
+		delete document.body.style.oldCSS;
+	}
+	else {
+		//	turn on
+		document.body.style.oldCSS = document.body.style.cssText;
+		var getting = browser.storage.local.get('maxWidth');
+		getting.then(onGot, onError);
+	}
+	browser.runtime.onMessage.removeListener(toggleWidth);
 
-		function onGot(item) {
-			var maxWidth=item.maxWidth||960;
-			document.body.style.cssText=`max-width: ${maxWidth}px; margin: 0 auto; position: relative;`;
-		}
-		function onError(error) {
-			console.log(`Error: ${error}`);
-		}
-
+	function onGot(item) {
+		var maxWidth = item.maxWidth || 960;
+		document.body.style.cssText=`max-width: ${maxWidth}px; margin: 0 auto; position: relative;`;
+	}
+	function onError(error) {
+		console.log(`Error: ${error}`);
 	}
 
-	browser.runtime.onMessage.addListener(toggleWidth);
+}
+
+browser.runtime.onMessage.addListener(toggleWidth);
